d9bc3d8b59f0a9141db9fb3cbcf367b9cc0f8c0b
Atapi <34801996+Sterophonick@users.noreply.github.com>
2024-08-27 22:11:08 -0600

    Update template.c

--- source/template.c
+++ source/template.c
@@ -1,9 +1,22 @@
 #include <hrt_video.h>
+#include <hrt_bios.h>
 #include "template.h"
+#include "l1_m3_lz_bmp.h"
+
+u32 tempVar = 0;
 
 int main(void)
 {
 	hrt_Init();
-	REG_DISPCNT &= 0xFF7F;
+	hrt_DSPSetBGMode(3);
+	hrt_DSPEnableBG(2);
+	hrt_DSPDisableForceBlank();
+	hrt_EnableGreenSwap();
+	hrt_LZ77UnCompVRAM((void*)l1Bitmap, (void*)VRAM);
+	while(1)
+	{
+		hrt_VBlankIntrWait();
+		tempVar++;
+	}
 	return 0;
 }
